bc6baea5a079c8926ad5e30116903be07c104c48
scott <scott.matthews343@gmail.com>
2023-11-16 19:01:03 +0000

    bootstrap ui

--- mvp/src/main.rs
+++ mvp/src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_web::web::Redirect;
 
 use qrcode::render::svg;
 use qrcode::{EcLevel, QrCode, Version};
 use tera::{Tera, Context};
 use serde::Deserialize;
+use rand::{distributions::Alphanumeric, Rng};
+use log::debug;
 
 type AppData = web::Data<AppState<MemoryStampCardTracker>>;
 
@@ -18,8 +21,8 @@ where T: StampCardTracker
 }
 
 trait StampCardTracker {
-    fn get_or_create_card(&mut self, card_id: StampCardId) -> &BasicStampCard;
-    fn stamp_card(&mut self, card_id: StampCardId) -> &BasicStampCard;
+    fn get_or_create_card(&mut self, card_id: &StampCardId) -> &BasicStampCard;
+    fn stamp_card(&mut self, card_id: &StampCardId) -> &BasicStampCard;
 }
 
 struct MemoryStampCardTracker {
@@ -35,18 +38,18 @@ impl MemoryStampCardTracker {
 }
 
 impl StampCardTracker for MemoryStampCardTracker {
-    fn get_or_create_card(&mut self, card_id: StampCardId) -> &BasicStampCard {
+    fn get_or_create_card(&mut self, card_id: &StampCardId) -> &BasicStampCard {
         if !self.store.contains_key(&card_id) {
-            self.store.insert(card_id, BasicStampCard::new());
+            self.store.insert(card_id.clone(), BasicStampCard::new());
         }
 
         self.store.get_key_value(&card_id).unwrap().1
     }
 
-    fn stamp_card(&mut self, card_id: StampCardId) -> &BasicStampCard {
+    fn stamp_card(&mut self, card_id: &StampCardId) -> &BasicStampCard {
         if !self.store.contains_key(&card_id) {
             let new_card = BasicStampCard::new();
-            self.store.insert(card_id, new_card);
+            self.store.insert(card_id.clone(), new_card);
             return self.store.get_key_value(&card_id).unwrap().1
         }
 
@@ -55,7 +58,7 @@ impl StampCardTracker for MemoryStampCardTracker {
             stamps: existing_card.stamps + 1,
             capacity: existing_card.capacity,
         };
-        self.store.insert(card_id, updated_card);
+        self.store.insert(card_id.clone(), updated_card);
         return self.store.get_key_value(&card_id).unwrap().1
     }
 }
@@ -63,9 +66,10 @@ impl StampCardTracker for MemoryStampCardTracker {
 trait StampCard {
     fn add_stamp(&self); // result?
 }
-#[derive(Hash, Eq, PartialEq, Copy, Clone)]
-struct StampCardId(u32);
+#[derive(Hash, Eq, PartialEq, Clone)]
+struct StampCardId(String);
 
+#[derive(Debug)]
 struct BasicStampCard {
     stamps: u32,
     capacity: u32,
@@ -74,7 +78,7 @@ struct BasicStampCard {
 impl BasicStampCard {
     fn new() -> BasicStampCard {
         BasicStampCard {
-            stamps: 0,
+            stamps: 1,
             capacity: 10
         }
     }
@@ -95,29 +99,37 @@ async fn hello() -> impl Responder {
 
 #[get("/qr")]
 async fn qr(data: AppData) -> impl Responder {
-    let code = QrCode::new(b"http://127.0.0.1:8080/hey").unwrap();
+    let code_id = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(7)
+        .map(char::from)
+        .collect::<String>();
+    let url = format!("http://127.0.0.1:8080/stamp/{code_id}");
+    let code = QrCode::new(url.as_bytes()).unwrap();
     let image = code
         .render()
         .min_dimensions(200, 200)
-        .dark_color(svg::Color("#800000"))
-        .light_color(svg::Color("#ffff80"))
+        .dark_color(svg::Color("#000000"))
+        .light_color(svg::Color("#ffffff"))
         .build();
 
     let mut ctx = Context::new();
     ctx.insert("qr", &image);
+    ctx.insert("url", &url);
 
     let rendered = data.tmpl.render("qr.html", &ctx).unwrap();
 
     HttpResponse::Ok().body(rendered)
 }
 
-#[get("/scan/{code}")]
-async fn scan(path: web::Path<u32>, data: AppData) -> HttpResponse {
+#[get("/stamp/{code}")]
+async fn stamp(path: web::Path<String>, data: AppData) -> HttpResponse {
     let code = path.into_inner();
     let mut ctx = Context::new();
-    ctx.insert("code", &code);
 
-    let rendered = data.tmpl.render("scan.html", &ctx).unwrap();
+    ctx.insert("code", &code);
+    let rendered = data.tmpl.render("stamp.html", &ctx).unwrap();
+
     HttpResponse::Ok().body(rendered)
 }
 
@@ -126,22 +138,34 @@ struct FormData {
     phone_number: String, // 11 ints?
 }
 
-#[post("/submit/{code}")]
-async fn submit(path: web::Path<u32>, data: web::Data<AppState<MemoryStampCardTracker>>, form: web::Form<FormData>) -> HttpResponse {
-    let code = path.into_inner();
-    let pn = &form.phone_number;
-    let mut tracker = data.tracker.lock().unwrap();
-    let user_card = tracker.stamp_card(StampCardId(123));
+#[post("/stamp/{code}")]
+async fn submit(path: web::Path<String>, data: web::Data<AppState<MemoryStampCardTracker>>, form: web::Form<FormData>) -> impl Responder {
+    let code_id = path.into_inner(); //  TODO this should trigger a refresh of the QR code
+    let user_id = &form.phone_number;
+    let card_id = StampCardId(user_id.clone());
 
-    println!("BasicStampCard with id {:?} now has {} stamps", 123, user_card.stamps);
-    HttpResponse::Ok().finish()
+    let mut tracker = data.tracker.lock().unwrap();
+    let user_card = tracker.stamp_card(&card_id);
+    let url = format!("http://127.0.0.1:8080/card/{}", user_id);
+
+    dbg!(user_card);
+    Redirect::to(url).temporary().see_other()
 }
 
 #[get("/card/{id}")]
-async fn display_card(path: web::Path<u32>, data: AppData) -> Result<String> {
+async fn display_card(path: web::Path<String>, data: AppData) -> HttpResponse {
+    let user_id = path.into_inner();
+    let card_id = StampCardId(user_id.clone());
+
     let mut tracker = data.tracker.lock().unwrap();
-    let card = tracker.get_or_create_card(StampCardId(123));
-    Ok(format!("Your card has {}/{} stamps", card.stamps, card.capacity))
+    let card = tracker.get_or_create_card(&card_id);
+
+    //Ok(format!("Your card has {}/{} stamps", card.stamps, card.capacity))
+
+    let mut ctx = Context::new();
+    ctx.insert("count", &card.stamps);
+    let rendered = data.tmpl.render("card.html", &ctx).unwrap();
+    HttpResponse::Ok().body(rendered)
 }
 
 #[get("/echo")]
@@ -177,7 +201,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .service(qr)
-            .service(scan)
+            .service(stamp)
             .service(submit)
             .service(display_card)
             .route("/hey", web::get().to(manual_hello))
